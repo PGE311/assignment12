e61b3bd3e3016e9bf178033955c0143c4a3d8fd1
John T. Foster <johntfosterjr@gmail.com>
2026-03-09 17:13:59 -0500

    Update for 2026

--- test.py
+++ test.py
@@ -52,26 +52,7 @@ class TestSolution(unittest.TestCase):
         A.row_combine(0, 1, -10)
         np.testing.assert_equal(A(), np.array([[31, 42], [3, 4]]))
 
-    def test_row_swap_private(self):
 
-        x = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        A = Matrix(x)
-        A.row_swap(1, 2)
-        np.testing.assert_equal(A(), np.array([[0, 1, 2], [6, 7, 8], [3, 4, 5]]))
-
-    def test_row_multiply_private(self):
-
-        x = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        A = Matrix(x)
-        A.row_multiply(1, 20)
-        np.testing.assert_equal(A(), np.array([[0, 1, 2], [60, 80, 100], [6, 7, 8]]))
-
-    def test_row_combine_private(self):
-
-        x = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        A = Matrix(x)
-        A.row_combine(2, 1, -12)
-        np.testing.assert_equal(A(), np.array([[ 0,  1,  2], [ 3,  4,  5], [42, 55, 68]]))
 
 
 if __name__ == '__main__':
